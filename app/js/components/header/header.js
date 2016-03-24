2b3466b4be0c913bfacf247dda0ac71100051141
timcmiller <timcmiller@live.com>
2016-03-24 12:25:13 -0700

    working modal

--- app/js/components/header/header.js
+++ app/js/components/header/header.js
@@ -1,22 +1,27 @@
 import React from 'react';
-
+import SkyLight from 'react-skylight';
+import ResumeStyles from './../../styles/resume';
 export default React.createClass({
 
   render() {
+
     return (
       <header className="main-header">
           <ul className="site-nav">
             <li>
-              <a href="#home">Home</a>
+              <a className="nav-link" href="#home">Home</a>
             </li>
             <li>
-              <a href="#projects">Projects</a>
+              <a className="nav-link" href="#projects">Projects</a>
             </li>
             <li>
-              <a>Resume</a>
+              <a className="nav-link" onClick={() => this.refs.customDialog.show()}>Resume</a>
+              <SkyLight dialogStyles={ResumeStyles} hideOnOverlayClicked ref="customDialog" title="A Custom Modal">
+                <img className="resume-img" src="images/resume.png" alt="Resume" />
+              </SkyLight>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <a className="nav-link" href="#contact">Contact</a>
             </li>
           </ul>
       </header>
