e43fa7b4cbd030fc35bcf859c46921cd37a55f7c
timcmiller <timcmiller@live.com>
2016-03-25 15:25:49 -0700

    added icon fonts for projects

--- app/js/lib/projects.js
+++ app/js/lib/projects.js
@@ -5,8 +5,9 @@ const projects = [
       description: 'React Native mobile app available on the ExponentJS platform for both Android and iOS. Developed with Redux to help manage the state of the application.',
       tech: ['JavaScript', 'React Native', 'Redux', 'ES6', 'ExponentJS', 'iOS', 'Android'],
       img: 'oscar-app.PNG',
-      link: '',
-      github: 'https://github.com/timcmiller/oscar_app',
+      links: [
+        {site: 'github', icon:'icon-github', link:'https://github.com/timcmiller/oscar_app'}
+        ],
       year: 2016,
       height: '200px'
     },
@@ -16,8 +17,11 @@ const projects = [
       description: 'Learning Management System built for programming and code driven education programs requested by my alma mater, Code Fellows.',
       tech: ['JavaScript', 'SASS', 'React', 'Redux', 'Mocha/Chai', 'Babel', 'Gulp', 'Express', 'MongoDB', 'Webpack'],
       img: 'cadet.png',
-      link: 'http://cadet-lms.herokuapp.com/',
-      github: 'https://github.com/cf-lms/lms',
+      links: [
+        {site:'web', icon:'icon-sphere', link:'http://cadet-lms.herokuapp.com/'},
+        {site:'github', icon:'icon-github', link:'https://github.com/cf-lms'},
+        {site:'npm', icon:'icon-npm', link:'https://www.npmjs.com/package/cadet-parser'}
+      ],
       year: 2015,
       width: '100px'
     },
@@ -27,8 +31,10 @@ const projects = [
       description: 'Multi-lingual, natural language processing web app that returns the hidden sentiment of plain text or tweets.',
       tech: ['JavaScript', 'Express', 'MongoDB', 'Mocha/Chai', 'TravisCI', 'Twitter API', 'Foundations'],
       img: 'lingo-lemur.png',
-      link: 'http://lingolemur.com',
-      github: 'https://github.com/timcmiller/nlp_project',
+      links: [
+        {site:'web', icon:'icon-sphere', link:'http://lingolemur.com'},
+        {site:'github', icon:'icon-github', link:'https://github.com/timcmiller/lms'}
+        ],
       year: 2015
     },
     {
@@ -37,8 +43,10 @@ const projects = [
       description: 'Node backed framework published as an npm package that streamlines http server creation.',
       tech: ['JavaScript', 'Gulp', 'Mocha/Chai'],
       img: 'stg.png',
-      link: 'http://npmjs.com/sludgy-trucker-coffee',
-      github: 'https://github.com/timcmiller/sludgy-trucker-coffee',
+      links: [
+        {site:'github', icon:'icon-github', link:'https://github.com/timcmiller/sludgy-trucker-coffee'},
+        {site:'npm', icon:'icon-npm', link:'http://npmjs.com/sludgy-trucker-coffee'}
+        ],
       year: 2015,
       width: '200px'
     }
