05100f713b7ff46ba4b72e8847977a8064416903
timcmiller <timcmiller@live.com>
2016-04-08 12:07:37 -0700

    removed serve-faviocn and have it serving iteslef

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
-var favicon = require('serve-favicon');
 var contactRouter = require(__dirname + '/routes/contact');
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/build/'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
